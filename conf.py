ddf9175d0135d7aaf5f214aa4b478dca23859e5d
arielvb <i@arielvb.com>
2013-02-13 23:16:12 +0100

    Default theme at RTD

--- conf.py
+++ conf.py
@@ -13,6 +13,13 @@
 
 import sys, os
 
+# Readthedocs behavior for the theme (https://read-the-docs.readthedocs.org/en/latest/faq.html)
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -91,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+# html_theme = 'nature' # see on_rtd
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
